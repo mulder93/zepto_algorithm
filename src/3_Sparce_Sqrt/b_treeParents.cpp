88132eae6c795a9af4c28f1ed81aa9cddbedd969
Boris Tsarev <boristsarev@gmail.com>
2016-07-03 13:52:44 +0300

    Tree parents task

--- src/3_Sparce_Sqrt/b_treeParents.cpp
+++ src/3_Sparce_Sqrt/b_treeParents.cpp
@@ -2,93 +2,48 @@
 #include <vector>
 #include <cmath>
 #include <algorithm>
-#include <map>
 
-//class PowerOfTwoUtils
-//{
-//private:
-//	int m_maxValue;
-//	std::map<int, int> m_powers;
-//
-//public:
-//	PowerOfTwoUtils(int maxValue) : m_maxValue(maxValue)
-//	{
-//		fillPowers();
-//	}
-//
-//	static bool isPowerOfTwo(int value)
-//	{
-//		return (value & (value - 1)) == 0;
-//	}
-//
-//	int getMaxPowerOfTwoComponent(int value)
-//	{
-//		if (value > 0) {
-//			if (isPowerOfTwo(value)) {
-//				return value;
-//			} else {
-//				return (*(--m_powers.lower_bound(value))).first;
-//			}
-//		} else {
-//			return 0;
-//		}
-//	}
-//
-//	int getPowerForValue(int value)
-//	{
-//		if (m_powers.find(value) != m_powers.end()) {
-//			return m_powers[value];
-//		} else {
-//			return 0;
-//		}
-//		
-//	}
-//
-//private:
-//	void fillPowers()
-//	{
-//		auto power = 0;
-//		auto powerValue = 1;
-//		while (power <= m_maxValue) {
-//			m_powers[powerValue] = power;
-//			++power;
-//			powerValue *= 2;
-//		}
-//	}
-//};
+struct TreeNode
+{
+	int num;
+	std::vector<int> children;
+	std::vector<int> parents;
+
+	TreeNode() : num(0) { }
+};
+
+using Tree = std::vector<TreeNode>;
+
+void fillParents(Tree& tree, int node, std::vector<int>& parents)
+{
+	for (auto height = 2; height <= parents.size(); height *= 2) {
+		tree[node].parents.push_back(parents[parents.size() - (height - 1) - 1]);
+	}
+	parents.push_back(node);
+	for (auto child : tree[node].children) {
+		fillParents(tree, child, parents);
+	}
+	parents.pop_back();
+}
 
 void doTask3B()
 {
-	using Vector = std::vector<int>;
-	using Vector2D = std::vector<Vector>;
-
 	// Get params
 	auto nodeCount = 0;
 	auto queryCount = 0;
 	scanf("%d %d", &nodeCount, &queryCount);
 
 	// Read tree
-	Vector2D tree(nodeCount);
-	int firstParent = 0;
+	Tree tree(nodeCount);
+	auto parentIndex = 0;
 	for (auto i = 1; i < nodeCount; ++i) {
-		scanf("%d", &firstParent);
-		tree[i].push_back(firstParent);
+		scanf("%d", &parentIndex);
+		tree[i].num = i;
+		tree[i].parents.push_back(parentIndex);
+		tree[parentIndex].children.push_back(i);
 	}
-
-	// Save info about parents
-	auto height = 0;
-	auto parent = 0;
-	for (auto i = 1; i < nodeCount; ++i) {
-		height = 1;
-		parent = tree[i][0];
-		while (parent != 0) {
-			++height;
-			parent = tree[parent][0];
-			if ((height & (height -1)) == 0) {
-				tree[i].push_back(parent);
-			}
-		}
-	}
+	std::vector<int> parents;
+	fillParents(tree, 0, parents);
 
 	struct PowerOfTwo
 	{
@@ -113,7 +68,7 @@ void doTask3B()
 	auto currentNode = 0;
 	auto powerOfTwoComponent = 0;
 	auto power = 0;
-	height = 0;
+	auto height = 0;
 
 	for (auto i = 0; i < queryCount; ++i) {
 		scanf("%d %d", &node, &height);
@@ -122,8 +77,8 @@ void doTask3B()
 		while (height != 0) {
 			powerOfTwoComponent = twoPowers[height].value;
 			power = twoPowers[height].power;
-			if (tree[currentNode].size() > power) {
-				currentNode = tree[currentNode][power];
+			if (tree[currentNode].parents.size() > power) {
+				currentNode = tree[currentNode].parents[power];
 				height -= powerOfTwoComponent;
 			} else {
 				currentNode = -1;
@@ -133,9 +88,6 @@ void doTask3B()
 
 		printf("%d ", currentNode);
 	}
-
-	int stop;
-	scanf("%d", &stop);
 }
 
 int main()
